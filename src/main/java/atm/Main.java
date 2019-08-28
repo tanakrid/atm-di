c7579da4c203ef477e8bd6754469cf505a315543
chiney <tanakrid.c@ku.th>
2019-08-29 00:20:56 +0700

    first commit

--- src/main/java/atm/Main.java
+++ src/main/java/atm/Main.java
@@ -1,11 +1,12 @@
 package atm;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        DataSource dataSource = new DataSource("customers.txt");
-        Bank bank = new Bank(dataSource);
-        ATM atm = new ATM(bank);
-        ATMSimulator atmSimulator = new ATMSimulator(atm);
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        ATMSimulator atmSimulator = context.getBean("simulator", ATMSimulator.class);
         atmSimulator.run();
     }
 }
